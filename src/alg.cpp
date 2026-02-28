acffdb72c6697ee7cdbcc8d1637430423b8a528e
lerklmao <owins.12KVE@gmail.com>
2026-02-28 22:53:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,8 +6,8 @@
 bool checkPrime(uint64_t value) {
   if (value <=1) return false;
   if (value <=3) return true;
-  for (uint64_t i = 2; i < value; i++){
-    if (value % i == 0){
+  for (uint64_t i = 2; i < value; i++) {
+    if (value % i == 0) {
       return false;
     }
   }
@@ -20,9 +20,9 @@ uint64_t nPrime(uint64_t n) {
   }
   uint64_t number = 0;
   uint64_t now_number = 1;
-  while (number < n){
+  while (number < n) {
     now_number++;
-    if (checkPrime(now_number)){
+    if (checkPrime(now_number)) {
       number++;
     }
   }
@@ -31,9 +31,9 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
   uint64_t next_value = value;
-  while(true){
+  while(true) {
     next_value++;
-    if (checkPrime(next_value)){
+    if (checkPrime(next_value)) {
       return next_value;
     }
   }
@@ -41,17 +41,20 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  for(uint64_t i = 2; i< hbound; i++){
-    if (checkPrime(i)){
+  for(uint64_t i = 2; i< hbound; i++) {
+    if (checkPrime(i)) {
       sum+=i;
     }
   }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  uint64_t count =0;
-  for (uint64_t i = lbound; p + 2 < hbound; p++){
-    if (checkPrime(p) && checkPrime(p+2)){
+  uint64_t count = 0;
+  for (uint64_t i = lbound; i + 2 < hbound; i++) {
+    if (checkPrime(i) && checkPrime(i+2)) {
       count++;
+    }
   }
+  return count;
 }
