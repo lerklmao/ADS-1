164cf39a6bc4dbab3101f91d81a727c19d9776d0
lerklmao <owins.12KVE@gmail.com>
2026-02-28 23:12:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,7 +15,7 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-  if (n == 0){
+  if (n == 0) {
     return 0;
   }
   uint64_t number = 0;
@@ -31,7 +31,7 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
   uint64_t next_value = value;
-  while(true) {
+  while (true) {
     next_value++;
     if (checkPrime(next_value)) {
       return next_value;
@@ -41,7 +41,7 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  for(uint64_t i = 2; i< hbound; i++) {
+  for (uint64_t i = 2; i< hbound; i++) {
     if (checkPrime(i)) {
       sum+=i;
     }
